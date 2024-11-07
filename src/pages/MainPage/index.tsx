cabd15c1e0401bcb7700d7bada6a43b10ba77388
심은주 <nyaknya@naver.com>
2024-11-07 22:12:29 +0900

    ✨ feat: 메인 첫번째 섹션까지 완성

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,29 @@
+import classNames from 'classnames/bind';
+import styles from './MainPage.module.scss';
 import MainHeader from '../../components/Header/MainHeader';
 
+const cn = classNames.bind(styles);
+
 export default function MainPage() {
-  return <MainHeader />;
+  return (
+    <>
+      <MainHeader />
+      <main className={cn('main-wrap')}>
+        <div className={cn('main-content')}>
+          <div className={cn('main-txt')}>
+            <strong>Point. 01</strong>
+            <h2>
+              누구나 손쉽게, 온라인
+              <br />
+              롤링 페이퍼를 만들 수 있어요
+            </h2>
+            <p>로그인 없이 자유롭게 만들어요.</p>
+          </div>
+          <div className={cn('main-image')}>
+            <img src="/images/mainimg01.png" alt="롤링페이퍼 예시 사진" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
