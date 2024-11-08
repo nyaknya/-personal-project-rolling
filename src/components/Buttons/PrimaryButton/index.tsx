74cb7009859b062cb02c51c74cb6c752a68ccc63
심은주 <nyaknya@naver.com>
2024-11-09 00:25:19 +0900

    ✨ feat: 기본 버튼 컴포넌트 추가

---
+++ src/components/Buttons/PrimaryButton/index.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import classNames from 'classnames/bind';
+import styles from './PrimaryButton.module.scss';
+
+const cn = classNames.bind(styles);
+interface PrimaryButtonProps {
+  onClick: () => void;
+  children: ReactNode;
+  size?: 'L' | 'M' | 'S';
+  type?: 'button' | 'submit' | 'reset';
+}
+
+export default function PrimaryButton({
+  onClick,
+  children,
+  size = 'M',
+  type = 'button',
+}: PrimaryButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={cn('primary-button', {
+        large: size === 'L',
+        medium: size === 'M',
+        small: size === 'S',
+      })}
+    >
+      {children}
+    </button>
+  );
+}
