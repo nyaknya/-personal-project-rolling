541646faa5a240c57ff4e75df930372addd61d5f
심은주 <nyaknya@naver.com>
2024-11-10 21:24:24 +0900

    ✨ feat: 리스트페이지 슬라이드 재외 PC 완성

--- src/pages/ListPage/index.tsx
+++ src/pages/ListPage/index.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './ListPage.module.scss';
 import MainHeader from '../../components/Header/MainHeader';
 import CardList from './CardList';
+import PrimaryButton from '../../components/Buttons/PrimaryButton';
 
 const cn = classNames.bind(styles);
 
 export default function ListPage() {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate('/post');
+  };
+
   return (
     <>
       <MainHeader />
@@ -17,6 +24,11 @@ export default function ListPage() {
         <section className={cn('list-content')}>
           <h2>최근에 만든 롤링 페이퍼 ⭐️️</h2>
           <CardList />
+        </section>
+        <section className={cn('button-area')}>
+          <PrimaryButton onClick={handleButtonClick}>
+            나도 만들어보기
+          </PrimaryButton>
         </section>
       </main>
     </>
