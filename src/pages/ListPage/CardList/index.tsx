93d7cc8a6d3ce8c077663043dc8eecbd72acadc7
심은주 <nyaknya@naver.com>
2024-11-10 16:57:39 +0900

    ✨ feat: 카드리스트 불러오기 추가

--- src/pages/ListPage/CardList/index.tsx
+++ src/pages/ListPage/CardList/index.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './CardList.module.scss';
+import apiRequest from '../../../utils/apiRequest';
 
 const cn = classNames.bind(styles);
+
 export default function CardList() {
+  const [cardlist, setCardlist] = useState(null);
+
+  const getCardlist = async () => {
+    try {
+      const data = await apiRequest({ endpoint: '/recipients/' });
+      setCardlist(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCardlist();
+  }, []);
+
   return (
     <div className={cn('card-list-wrap')}>
       <ul>
