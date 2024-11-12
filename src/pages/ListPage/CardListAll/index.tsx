1c348e512e0c5a9711f8e7cf79d876e4396ba15e
심은주 <nyaknya@naver.com>
2024-11-12 21:20:25 +0900

    ✨ feat: cardlistall 컴포넌트 복사 및 추가

---
+++ src/pages/ListPage/CardListAll/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useCallback } from 'react';
+import classNames from 'classnames/bind';
+import styles from './CardList.module.scss';
+import apiRequest from '../../../utils/apiRequest';
+import Card from '../Card';
+import { CardListResultData } from '../../../types';
+
+const cn = classNames.bind(styles);
+
+interface CardListProps {
+  sort?: string;
+}
+
+export default function CardListAll({ sort }: CardListProps) {
+  const [cardlist, setCardlist] = useState<CardListResultData[]>([]);
+
+  const getCardlist = useCallback(async () => {
+    try {
+      const endpoint = `/recipients/?limit=15&${sort ? `sort=${sort}` : ''}`;
+      const data = await apiRequest({ endpoint });
+      setCardlist(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [sort]);
+
+  useEffect(() => {
+    getCardlist();
+  }, [getCardlist]);
+
+  return (
+    <div className={cn('card-list-wrap')}>
+      <ul className={cn('card-list')}>
+        {cardlist?.map((list) => (
+          <li key={list.id}>
+            <Card data={list} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
