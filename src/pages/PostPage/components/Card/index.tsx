8a1ff48df4dfe9eb54b885047ab1fb0c839ca214
심은주 <nyaknya@naver.com>
2024-11-19 20:11:07 +0900

    ✨ feat: 포스트페이지 카드리스트, 카드 컴포넌트 생성

---
+++ src/pages/PostPage/components/Card/index.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind';
+import styles from './Card.module.scss';
+const cn = classNames.bind(styles);
+
+export default function Card() {
+  return (
+    <div>
+      <span>카드</span>
+    </div>
+  );
+}
---
+++ src/pages/PostPage/components/CardList/index.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './CardList.module.scss';
+const cn = classNames.bind(styles);
+
+export default function CardList() {
+  return (
+    <div>
+      <ul>
+        <li>카드리스트(map 돌릴 예정)</li>
+      </ul>
+    </div>
+  );
+}
