e0e8608709cf4c81183d4cc5675d04e5f00472b3
심은주 <nyaknya@naver.com>
2024-11-26 18:34:28 +0900

    ✨ feat: 리스트페이지 전체 카드 더불러오기 추가

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -11,27 +11,31 @@ export default function useInfiniteScroll({
   isLoading,
   hasNextPage,
 }: UseInfiniteScrollProps) {
-  const observerRef = useRef<HTMLDivElement | null>(null);
+  const observerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const currentObserverRef = observerRef.current;
+    if (!observerRef.current) return undefined;
+
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && !isLoading && hasNextPage) {
           onIntersect();
         }
       },
-      { threshold: 1.0 },
+      {
+        threshold: 0.8,
+        rootMargin: '100px',
+      },
     );
 
-    if (currentObserverRef) {
-      observer.observe(currentObserverRef);
-    }
+    const currentTarget = observerRef.current;
+    observer.observe(currentTarget);
 
     return () => {
-      if (currentObserverRef) {
-        observer.unobserve(currentObserverRef);
+      if (currentTarget) {
+        observer.unobserve(currentTarget);
       }
+      return undefined;
     };
   }, [onIntersect, isLoading, hasNextPage]);
 
--- src/pages/ListPage/components/CardListAll/index.tsx
+++ src/pages/ListPage/components/CardListAll/index.tsx
@@ -1,38 +1,74 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './CardListAll.module.scss';
 import apiRequest from '../../../../utils/apiRequest';
 import Card from '../Card';
 import { CardListResultData } from '../../../../types';
+import PrimaryButton from '../../../../components/Buttons/PrimaryButton';
 
 const cn = classNames.bind(styles);
 
 export default function CardListAll() {
   const [cardlist, setCardlist] = useState<CardListResultData[]>([]);
+  const [offset, setOffset] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const limit = 8;
 
-  const getCardlist = useCallback(async () => {
+  const fetchInitialData = async () => {
     try {
-      const endpoint = '/recipients/?limit=20';
+      setIsLoading(true);
+      const endpoint = `/recipients/?limit=${limit}&offset=0`;
       const data = await apiRequest({ endpoint });
       setCardlist(data.results);
-    } catch (error) {
-      console.error(error);
+      setOffset(limit);
+      if (data.results.length < limit) setHasMore(false);
+    } finally {
+      setIsLoading(false);
     }
-  }, []);
+  };
+
+  const fetchMoreData = async () => {
+    if (isLoading || !hasMore) return;
+
+    try {
+      setIsLoading(true);
+      const endpoint = `/recipients/?limit=${limit}&offset=${offset}`;
+      const data = await apiRequest({ endpoint });
+      setCardlist((prev) => [...prev, ...data.results]);
+      setOffset((prevOffset) => prevOffset + limit);
+      if (data.results.length < limit) setHasMore(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    getCardlist();
-  }, [getCardlist]);
+    fetchInitialData();
+  }, []);
+
+  const handleLoadMore = () => {
+    fetchMoreData();
+  };
 
   return (
     <div className={cn('card-list-wrap')}>
       <ul className={cn('card-list')}>
-        {cardlist?.map((list) => (
+        {cardlist.map((list) => (
           <li key={list.id}>
             <Card data={list} />
           </li>
         ))}
       </ul>
+      <div className={cn('button-section')}>
+        {hasMore ? (
+          <PrimaryButton onClick={handleLoadMore} disabled={isLoading}>
+            {isLoading ? '불러오는 중...' : '더 불러오기'}
+          </PrimaryButton>
+        ) : (
+          <span>끝이에요! 😊</span>
+        )}
+      </div>
     </div>
   );
 }
