e33048ff3f730e9358a0d6cf229c388d04aab4dd
심은주 <nyaknya@naver.com>
2024-11-30 10:47:03 +0900

    ✨ feat: 404 페이지 디자인 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from './App';
 
 if (!window.Kakao.isInitialized()) {
   window.Kakao.init('89c447f53ffcf78338c0628624fc3216');
-  console.log('Kakao initialized:', window.Kakao.isInitialized());
 }
 
 const root = ReactDOM.createRoot(
--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,3 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './NotFoundPage.module.scss';
+import PrimaryButton from '../../components/Buttons/PrimaryButton';
+
+const cn = classNames.bind(styles);
+
 export default function NotFoundPage() {
-  return <span>페이지를 찾을 수 없습니다.</span>;
+  const navigate = useNavigate();
+  const backToReturn = () => {
+    navigate(-1);
+  };
+
+  return (
+    <main>
+      <div className={cn('notfound-content')}>
+        <h2>
+          404 <br />
+          PAGE NOT FOUND
+        </h2>
+        <p>페이지를 찾을 수 없습니다.</p>
+        <PrimaryButton onClick={backToReturn}>이전으로 돌아가기</PrimaryButton>
+      </div>
+    </main>
+  );
 }
